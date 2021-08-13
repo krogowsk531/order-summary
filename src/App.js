a4c33f7099b6bf14994c0b6c965c3b91a2abf76a
Katy St Sauveur <katyrogowski@yahoo.com>
2021-08-13 16:58:14 -0600

    Built wavy background

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='container'>
+      <div>
+        <svg viewBox='0 0 500 500'
+          perserveAspectRatio='xMinyMin meet'>
+          <path d='M0, 100 C150, 200, 350, 0 500, 100 L500, 00 L0, 0 Z' style={{stroke: 'none', fill: '#D6E1FF'}} >
+          </path>
+        </svg>
+      </div>
+    </section>
   );
 }
 

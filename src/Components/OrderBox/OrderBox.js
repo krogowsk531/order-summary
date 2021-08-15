f3ba017fd3b610a1c49615efbf6e4b8869bfee9f
Katy St Sauveur <katyrogowski@yahoo.com>
2021-08-15 14:54:20 -0600

    Imported image to use for summary card

---
+++ src/Components/OrderBox/OrderBox.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Component } from 'react';
+import humanImage from './illustration-hero.svg'
+
+class MattressSize extends Component {
+  // constructor() {
+  //   super()
+  // }
+  
+  render() {
+    return (
+      <div className='rectangle'>
+        <img src={humanImage} className='human-image' alt='human hero' />
+      </div>
+    )
+  }
+  )
+}
+
+export default OrderBox;

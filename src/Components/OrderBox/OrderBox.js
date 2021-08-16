df2be22ec1b2bb23f63d2a2ba0c436b72c01cc7f
Katy St Sauveur <katyrogowski@yahoo.com>
2021-08-15 20:23:43 -0600

    Styled about app text

--- src/Components/OrderBox/OrderBox.js
+++ src/Components/OrderBox/OrderBox.js
@@ -7,6 +7,7 @@ class OrderBox extends Component {
       <div className='rectangle'>
         <img src={humanImage} className='human-image' alt='human hero' />
         <p className='order-summary-text'>Order Summary</p>
+        <p className='about-app'>You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!</p>
         <button className='payment-button'>Proceed to Payment</button>
       </div>
     )

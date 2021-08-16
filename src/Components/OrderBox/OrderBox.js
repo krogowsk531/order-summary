dccceb21af8e3992c52fd3b89abbb5ba626af0f1
Katy St Sauveur <katyrogowski@yahoo.com>
2021-08-15 20:38:21 -0600

    Add cancel order text

--- src/Components/OrderBox/OrderBox.js
+++ src/Components/OrderBox/OrderBox.js
@@ -9,6 +9,7 @@ class OrderBox extends Component {
         <p className='order-summary-text'>Order Summary</p>
         <p className='about-app'>You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!</p>
         <button className='payment-button'>Proceed to Payment</button>
+        <p className='cancel-order'>Cancel Order</p>
       </div>
     )
   }

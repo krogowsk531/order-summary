684046e953bc6aaa18826ac6f8a8b064d13406f2
Katy St Sauveur <katyrogowski@yahoo.com>
2021-08-19 10:27:09 -0600

    Rectangle appears complete

--- src/Components/OrderBox/OrderBox.js
+++ src/Components/OrderBox/OrderBox.js
@@ -6,10 +6,17 @@ class OrderBox extends Component {
     return (
       <div className='rectangle'>
         <img src={humanImage} className='human-image' alt='human hero' />
+        <div className='bottom-third'>
         <p className='order-summary-text'>Order Summary</p>
         <p className='about-app'>You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!</p>
+          <div className='grey-box'>
+            <p>Annual Plan</p>
+            <p>$59.99/year</p>
+            <p>Change</p>
+          </div>
         <button className='payment-button'>Proceed to Payment</button>
         <p className='cancel-order'>Cancel Order</p>
+      </div>
       </div>
     )
   }
